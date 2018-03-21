a4bbf605ea70de972e74b6bbb34f9ece4d7ca27e
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-03-21 15:20:53 -0500

    complete

---
+++ app/booked/bookedCtrl.js
@@ -0,0 +1,16 @@
+angular.module('devmtnTravel').controller('bookedCtrl', function($scope, $stateParams, mainSrvc ){
+    // $scope.test = test;
+    mainSrvc.getPackageInfo().then( function( response ){
+        $scope.allPackages = response.data;
+
+        if( $stateParams.id ) {
+            $scope.packageIndex = $scope.allPackages.findIndex( function( package ){
+                return package.id === parseInt( $stateParams.id );
+            })
+
+        if( $scope.packageIndex !== -1 ){
+            $scope.package = $scope.allPackages[ $scope.packageIndex ];
+        }
+        }
+    })
+})
---
+++ app/packages/packagesCtrl.js
@@ -0,0 +1,12 @@
+angular.module('devmtnTravel').controller('packagesCtrl', function($scope, $stateParams, mainSrvc){
+    // $scope.test = test;
+    mainSrvc.getPackageInfo().then( function( response ) {
+    $scope.allPackages = response.data;
+
+    if( $stateParams.country ){
+        $scope.packages = $scope.allPackages.filter( function( package ){
+            return package.country === $stateParams.country;
+        })
+    }
+})
+})
